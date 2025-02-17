90442ced7adbb92d33deeb5b72eda6f60729f200
ItsZmainDev <zmainpro@gmail.com>
2025-02-17 11:51:43 +0100

    Task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a specific message
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0)
+}
