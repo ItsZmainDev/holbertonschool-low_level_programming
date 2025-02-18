44e7db9d165a0aad9dc467e3e2cff242833b33dc
ItsZmainDev <zmainpro@gmail.com>
2025-02-18 18:07:12 +0100

    Task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Display the alphabet with putchar whithout q & e letter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabt[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (alphabt[i] != '\0')
+	{
+		if (alphabt[i] != "q" && alphabt[i] != "e")
+		{
+			putchar(alphabt[i];
+		}
+
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
