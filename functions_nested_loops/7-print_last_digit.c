2a71c000391dd278d43d5b51f41ca3d5d8ea4fec
ItsZmainDev <zmainpro@gmail.com>
2025-02-20 20:32:35 +0100

    Task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to check
+ *
+ * Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+
+	return (last_digit);
+}
