93794a0379fab73d2fcd2bdaaa1343fa1a10d1f5
ItsZmainDev <zmainpro@gmail.com>
2025-02-20 13:48:24 +0100

    Task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints Alphabet
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
+
+/**
+ * print_alphabet_x10 - Prints Alphabet with alphabet function
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}
