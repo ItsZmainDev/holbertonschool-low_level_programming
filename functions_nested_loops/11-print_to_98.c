7b57b2fe8472dda3456382e038b200597841cdc7
ItsZmainDev <zmainpro@gmail.com>
2025-02-21 10:58:23 +0100

    Task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		_putchar(n);
+		for (i = n + 1; i >= 98; i--)
+		{
+			_putchar(',')
+			_putchar(i + '0');
+		}
+	}
+	else
+	{
+		_putchar(n);
+		for (i = n + 1; i <= 98; i++)
+		{
+			_putchar(',')
+			_putchar(i + '0');
+		}
+	}
+}
