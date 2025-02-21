20260ae8cd2a2b06ec5b103d656f8b2a5a7d7207
ItsZmainDev <zmainpro@gmail.com>
2025-02-21 15:15:00 +0100

    Task 12 - Correction

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,34 +1,6 @@
 #include "main.h"
 
 /**
- * print_number - prints a number with the correct formatting
- * @num: the number to print
- *
- * Return: void
- */
-void print_number(int num)
-{
-    if (num < 10)
-    {
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(num + '0');
-    }
-    else if (num < 100)
-    {
-        _putchar(' ');
-        _putchar((num / 10) + '0');
-        _putchar((num % 10) + '0');
-    }
-    else
-    {
-        _putchar((num / 100) + '0');
-        _putchar(((num / 10) % 10) + '0');
-        _putchar((num % 10) + '0');
-    }
-}
-
-/**
  * print_times_table - prints the n times table, starting with 0
  * @n: the number of times table to print
  *
@@ -36,31 +8,42 @@ void print_number(int num)
  */
 void print_times_table(int n)
 {
-    int result;
-    int i, j;
+	int result = 0;
+	int i, j;
 
-    if (n < 0 || n > 15)
-    {
-        return;
-    }
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            result = i * j;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
 
-            if (j == 0)
-            {
-                _putchar(result + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                print_number(result);
-            }
-        }
-        _putchar('\n');
-    }
+			if (j == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (result < 10)
+				{
+					_putchar(' ');
+				}
+				else if (result < 100)
+				{
+					_putchar(' ');
+				}
+
+				_putchar(result + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
 }
