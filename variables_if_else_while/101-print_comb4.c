34b39b538ef0d440806efb373bca874d3ad8548c
ItsZmainDev <zmainpro@gmail.com>
2025-02-21 14:28:21 +0100

    Task 11 - Correction

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -21,9 +21,9 @@ int main(void)
 
 			while (k < 10)
 			{
-				putchar(i + 0);
-				putchar(j + 0);
-				putchar(k + 0);
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 
 				if (i != 7 || j != 8 || k != 9)
 				{
