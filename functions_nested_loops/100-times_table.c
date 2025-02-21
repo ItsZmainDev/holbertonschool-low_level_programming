02d45ae2fec8b04684e1f625026f8953540ddf2b
ItsZmainDev <zmainpro@gmail.com>
2025-02-21 14:54:48 +0100

    Task 12 - Correction

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -8,42 +8,49 @@
  */
 void print_times_table(int n)
 {
-	int result = 0;
-	int i, j;
+    int result;
+    int i, j;
 
-	if (n < 0 || n > 15)
-	{
-		return;
-	}
+    if (n < 0 || n > 15)
+    {
+        return;
+    }
 
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			result = i * j;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            result = i * j;
 
-			if (j == 0)
-			{
-				_putchar(result + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
+            if (j == 0)
+            {
+                _putchar(result + '0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
 
-				if (result < 10)
-				{
-					_putchar(' ');
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-				}
-
-				_putchar(result + '0');
-			}
-		}
-
-		_putchar('\n');
-	}
+                if (result < 10)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(result + '0');
+                }
+                else if (result < 100)
+                {
+                    _putchar(' ');
+                    _putchar((result / 10) + '0');
+                    _putchar((result % 10) + '0');
+                }
+                else
+                {
+                    _putchar((result / 100) + '0');
+                    _putchar(((result / 10) % 10) + '0');
+                    _putchar((result % 10) + '0');
+                }
+            }
+        }
+        _putchar('\n');
+    }
 }
