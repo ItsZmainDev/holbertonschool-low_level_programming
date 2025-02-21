bd1ff96388481a13f5787360dbba75feb5f63cf9
ItsZmainDev <zmainpro@gmail.com>
2025-02-21 14:52:49 +0100

    Task 12 - Correction

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -14,10 +14,11 @@ void print_times_table(int n)
 	}
 
 	int result = 0;
+	int i, j;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			result = i * j;
 
