61ab89f1d14c8fc5a817179009b08e0d3f4d0a5e
ItsZmainDev <zmainpro@gmail.com>
2025-02-24 13:13:34 +0100

    Task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Print lines x n
+ * @n: The number
+ */
+void print_line(int n);
+{
+	int i;
+
+	if (n < 1)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
