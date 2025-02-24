fa0ba10a93a5ee1332e4277646c3c43c7cb22f67
ItsZmainDev <zmainpro@gmail.com>
2025-02-24 10:26:28 +0100

    Check the character is in uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Check the uppercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is upper or 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
