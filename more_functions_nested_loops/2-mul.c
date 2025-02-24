6213c9bcbec1fbe2b3eeed670c7539d3669fcca1
ItsZmainDev <zmainpro@gmail.com>
2025-02-24 11:28:03 +0100

    Task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: Integer 1
+ * @b: Interger 2
+ *
+ * Return: The result of the mul
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
