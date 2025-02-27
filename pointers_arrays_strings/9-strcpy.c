e7adbf48edf0780b8d231d1aedec874b636f15b5
ItsZmainDev <zmainpro@gmail.com>
2025-02-27 22:05:45 +0100

    Task 9 - Correction

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
  * @dest: pointer to the buffer in which to copy the string
  * @src: pointer to the string to copy
  *
