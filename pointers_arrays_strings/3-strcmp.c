774b604a42dcf988eec61c629c3705d3290647f6
ItsZmainDev <zmainpro@gmail.com>
2025-03-01 19:13:23 +0100

    Task 3 - Correrction

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result;
+	int i = 0;
 
-	if (s1 == s2)
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
-		result = 0;
-	}
-	else if (s1 < s2)
-	{
-		result = -15;
-	}
-	else
-	{
-		result = 15;
+		i++;
 	}
 
-	return (result);
+	return (s1[i] - s2[i]);
 }
