48113aa77f5f77e514add7a9bb61c3feacbad3d5
ItsZmainDev <zmainpro@gmail.com>
2025-03-01 21:25:44 +0100

    Task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: String to capitalize
+ *
+ * Return: Pointer to the string
+ */
+char *cap_string(char *str)
+{
+	int i = 0, cap_next = 1;
+
+	while (str[i] != '\0')
+	{
+		if (cap_next && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+
+		cap_next = 0;
+
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			capitalize_next = 1;
+		}
+	}
+
+	return (str);
+}
