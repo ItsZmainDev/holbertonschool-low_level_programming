6ebb0f2d1b4dd96eb3dc2f8faef464f6b4c2dcfb
ItsZmainDev <zmainpro@gmail.com>
2025-03-01 19:03:08 +0100

    Task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Difference between the two strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	if (s1 == s2)
+	{
+		result = 0;
+	}
+	else if (s1 < s2)
+	{
+		result = -15
+	}
+	else
+	{
+		result = 15
+	}
+
+	return (result)
+}
