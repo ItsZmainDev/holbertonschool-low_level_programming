03e3c09b6b8c7b33068aca60b0fd9b7ea71f84ad
ItsZmainDev <zmainpro@gmail.com>
2025-03-07 19:47:58 +0100

    Task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = argv[1] * argv[2];
+
+	return (i);
+}
