78bdec59628d4fcfff9a7b45386e6e0e65186d63
ItsZmainDev <zmainpro@gmail.com>
2025-03-14 22:55:16 +0100

    Task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog
+ * @d: pointer to dog
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
