dae47b17bfbc5b7a2caff510925aa17bf407f687
ItsZmainDev <zmainpro@gmail.com>
2025-03-14 22:39:33 +0100

    Fix structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,14 @@
+
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: dog structure
+ */
 struct dog {
-	char * name,
+	char *name,
 	float age,
-	char * owner
+	char *owner
 }
